b53bd4b9125127e1eb4abb8475e0292a2b303f9e
Karol Krakowiak <karol.krakowiak@gmail.com>
2023-12-17 13:28:52 +0100

    Refactor CSS class in Link.astro and add 'xs' screen size in Tailwind config
    Updated a specific CSS class in Link.astro file to better handle smaller screens. Also, added 'xs' screen size in Tailwind configuration to ensure responsiveness across different devices especially for smaller screens.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Figtree', 'sans-serif'],
-				heading: ['CabinetGrotesk-Variable', 'sans-serif'],
-			},
-			colors: {
-				'brand': '#1F01B9',
-				'ladniejszyinternet': '#F8D20F',
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', 'sans-serif'],
+                heading: ['CabinetGrotesk-Variable', 'sans-serif'],
+            },
+            colors: {
+                'brand': '#1F01B9',
+                'ladniejszyinternet': '#F8D20F',
+            },
+            screens: {
+                'xs': '320px',
+            }
+        },
+    },
+    plugins: [],
 }
